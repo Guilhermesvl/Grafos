55128b3ac67dd80459af4d3287bb99b354e253c6
Guilherme Silva <108199853+Guilhermesvl@users.noreply.github.com>
2023-04-28 22:56:18 -0300

    "conversão" questão 2 .cpp para .py

---
+++ exerciciosAula/questao2MatrizAdjacenciaPython.py
@@ -0,0 +1,26 @@
+def retornaTransposto(MA, n):
+    LA = [[] for _ in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            if (MA[i][j] == 1):
+                LA[j].append(n)
+
+    return LA
+
+n, m = map(int, input())
+
+while True:
+    MA = [[0 for j in range(n)] for i in range(n)]     #matriz nxn inicializada com 0
+
+    for i in range(m):
+        u, v = map(int, input())
+        MA[u][v] = 1
+
+    LA = retornaTransposto(MA, n)
+    for u in range(n):
+        for j in LA[u]:
+            v = LA[u][j]
+            print(v, " ")
+        print()
+            
