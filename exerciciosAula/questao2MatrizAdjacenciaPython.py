472b222e706a19f0cece7ac865ba82ff257eec89
Guilherme Silva <108199853+Guilhermesvl@users.noreply.github.com>
2023-05-03 22:03:56 -0300

    correção questão 2

--- exerciciosAula/questao2MatrizAdjacenciaPython.py
+++ exerciciosAula/questao2MatrizAdjacenciaPython.py
@@ -2,10 +2,9 @@ def retornaTransposto(MA, n):
     LA = [[] for _ in range(n)]
 
     for i in range(n):
-        j = i
-        for j in range(n):
+        for j in range(i, n):
             if (MA[i][j] == 1):
-                LA[j].append(n)
+                LA[j].append(i)
 
     return LA
 
@@ -15,13 +14,9 @@ while True:
     MA = [[0 for j in range(n)] for i in range(n)]     #matriz nxn inicializada com 0
 
     for i in range(m):
-        u, v = map(int, input().split)
+        u, v = map(int, input().split())
         MA[u][v] = 1
 
     LA = retornaTransposto(MA, n)
-    for u in range(n):
-        for j in LA[u]:
-            v = LA[u][j]
-            print(v, " ")
-        print()
-            
+
+    print(LA)
